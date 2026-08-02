150c6dac95b2cc37b4a928f46d5537b8218533ef
derrchan2codies <derrchan1@gmail.com>
2026-08-02 12:35:07 +0800

    Add files via upload

---
+++ types/env.d.ts
@@ -0,0 +1,9 @@
+declare const process: {
+  env: {
+    OPENAI_API_KEY?: string;
+    OPENAI_BASE_URL?: string;
+    OPENAI_MODEL?: string;
+    NETLIFY_AI_GATEWAY_BASE_URL?: string;
+    NETLIFY_AI_GATEWAY_KEY?: string;
+  };
+};
