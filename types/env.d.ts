53977f227aa0d86d3f8ea0afc12051b3f406f19a
derrchan2codies <derrchan1@gmail.com>
2026-08-15 15:51:49 +0800

    Delete types directory

--- types/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare const process: {
-  env: {
-    OPENAI_API_KEY?: string;
-    OPENAI_BASE_URL?: string;
-    OPENAI_MODEL?: string;
-    NETLIFY_AI_GATEWAY_BASE_URL?: string;
-    NETLIFY_AI_GATEWAY_KEY?: string;
-  };
-};
